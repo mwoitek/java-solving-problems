5a52ffc152ad8d749e377796e2e7d636994d6f92
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-24 12:25:45 -0300

    1st assignment, part 1

--- week_2/src/solution_first/Part1.java
+++ week_2/src/solution_first/Part1.java
@@ -2,8 +2,51 @@ package solution_first;
 
 public class Part1 {
 
+  public String findSimpleGene(String dna) {
+    int idxStart = dna.indexOf("ATG");
+    if (idxStart == -1) {
+      return "";
+    }
+    int idxStop = dna.indexOf("TAA", idxStart + 3);
+    if (idxStop == -1) {
+      return "";
+    }
+    String gene = dna.substring(idxStart, idxStop + 3);
+    if (gene.length() % 3 == 0) {
+      return gene;
+    }
+    return "";
+  }
+
+  public void testSimpleGene() {
+    String dna = "TTGGGTTAAGTC";
+    System.out.println("DNA: " + dna);
+    System.out.println("Gene: " + findSimpleGene(dna));
+    System.out.println();
+
+    dna = "ATGGGTTGAGTC";
+    System.out.println("DNA: " + dna);
+    System.out.println("Gene: " + findSimpleGene(dna));
+    System.out.println();
+
+    dna = "TTGGGTTGAGTC";
+    System.out.println("DNA: " + dna);
+    System.out.println("Gene: " + findSimpleGene(dna));
+    System.out.println();
+
+    dna = "ATGGGTTAAGTC";
+    System.out.println("DNA: " + dna);
+    System.out.println("Gene: " + findSimpleGene(dna));
+    System.out.println();
+
+    dna = "GATGGTTAAGTC";
+    System.out.println("DNA: " + dna);
+    System.out.println("Gene: " + findSimpleGene(dna));
+  }
+
   public static void main(String[] args) {
-    //
+    Part1 p1 = new Part1();
+    p1.testSimpleGene();
   }
 
 }
