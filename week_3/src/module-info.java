83440a10a9c220d1a71af57e4462ed41b35647c7
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-25 17:55:24 -0300

    Week 3 Setup

---
+++ week_3/src/module-info.java
@@ -0,0 +1,4 @@
+module csv_files_basic_statistics {
+  requires java.desktop;
+  requires java.sql;
+}
---
+++ week_3/src/solution/ParsingExportData.java
@@ -0,0 +1,9 @@
+package solution;
+
+public class ParsingExportData {
+
+  public static void main(String[] args) {
+    //
+  }
+
+}
