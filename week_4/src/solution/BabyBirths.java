f9576c01667333e29513d78b49175a899eb09f6d
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-26 22:22:00 -0300

    finished parts 1 and 2

--- week_4/src/solution/BabyBirths.java
+++ week_4/src/solution/BabyBirths.java
@@ -5,38 +5,81 @@ import edu.duke.FileResource;
 
 public class BabyBirths {
 
-  public void printNames() {
-    FileResource fr = new FileResource();
-    for (CSVRecord rec : fr.getCSVParser(false)) {
-      int numBorn = Integer.parseInt(rec.get(2));
-      if (numBorn <= 100) {
-        System.out.println(
-            "Name " + rec.get(0) + " Gender " + rec.get(1) + " Num Born " + rec.get(2));
-      }
-    }
-  }
-
   public void totalBirths(FileResource fr) {
-    int totalBirths = 0;
-    int totalBoys = 0;
-    int totalGirls = 0;
-    for (CSVRecord rec : fr.getCSVParser(false)) {
-      int numBorn = Integer.parseInt(rec.get(2));
-      totalBirths += numBorn;
-      if (rec.get(1).equals("M")) {
-        totalBoys += numBorn;
+    int numBirths;
+    int numGirls = 0;
+    int numBoys = 0;
+    int numGirlNames = 0;
+    int numBoyNames = 0;
+    for (CSVRecord record : fr.getCSVParser(false)) {
+      numBirths = Integer.parseInt(record.get(2));
+      if (record.get(1).equals("F")) {
+        numGirls += numBirths;
+        numGirlNames++;
       } else {
-        totalGirls += numBorn;
+        numBoys += numBirths;
+        numBoyNames++;
       }
     }
-    System.out.println("total births = " + totalBirths);
-    System.out.println("female girls = " + totalGirls);
-    System.out.println("male boys = " + totalBoys);
+    System.out.println("GIRLS");
+    System.out.println("Number of births: " + numGirls);
+    System.out.println("Number of names: " + numGirlNames);
+    System.out.println("BOYS");
+    System.out.println("Number of births: " + numBoys);
+    System.out.println("Number of names: " + numBoyNames);
+    System.out.println("TOTAL");
+    System.out.println("Number of births: " + (numGirls + numBoys));
+    System.out.println("Number of names: " + (numGirlNames + numBoyNames));
   }
 
   public void testTotalBirths() {
-    FileResource fr = new FileResource("data/yob2014.csv");
+    FileResource fr = new FileResource("../data/baby_names/example-small.csv");
     totalBirths(fr);
+  }
+
+  public int getRank(int year, String name, String gender) {
+    int rank = 1;
+    boolean found = false;
+    FileResource fr = new FileResource("../data/baby_names/yob" + year + ".csv");
+    for (CSVRecord record : fr.getCSVParser(false)) {
+      if (record.get(1).equals(gender)) {
+        if (record.get(0).equals(name)) {
+          found = true;
+          break;
+        }
+        rank++;
+      }
+    }
+    if (found) {
+      return rank;
+    }
+    return -1;
+  }
+
+  public void testGetRank() {
+    int year = 2012;
+    String name = "Mason";
+    String gender = "M";
+    System.out.println("year = " + year + ", name = " + name + ", gender = " + gender);
+    System.out.println("rank = " + getRank(year, name, gender));
+
+    year = 1997;
+    name = "Sarah";
+    gender = "F";
+    System.out.println("year = " + year + ", name = " + name + ", gender = " + gender);
+    System.out.println("rank = " + getRank(year, name, gender));
+
+    year = 2012;
+    name = "xxxxx";
+    gender = "M";
+    System.out.println("year = " + year + ", name = " + name + ", gender = " + gender);
+    System.out.println("rank = " + getRank(year, name, gender));
+  }
+
+  public static void main(String[] args) {
+    BabyBirths bb = new BabyBirths();
+    // bb.testTotalBirths();
+    bb.testGetRank();
   }
 
 }
